8ca1af3d92ca44397ffec026209e92727ef69668
pederan <peder1976@gmail.com>
2017-11-21 12:11:28 +0100

    version number fix

--- hogan-table.php
+++ hogan-table.php
@@ -3,7 +3,7 @@
  * Plugin Name: Hogan Module: Table
  * Plugin URI: https://github.com/dekodeinteraktiv/hogan-table
  * Description: Table Module for Hogan.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Dekode
  * Author URI: https://dekode.no
  * License: GPL-3.0
